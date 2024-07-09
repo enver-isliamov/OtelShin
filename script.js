f581914475b1d6ba05328ff97c90c03069c97dab
enver-isliamov <enverphoto@gmail.com>
2024-07-09 17:58:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,40 @@
 var modalOpenBtn = document.getElementById('modal-open');
 var modal = document.getElementById('modal');
 var modalCloseBtn = document.getElementById('modal-close');
+var modalUnderstandBtn = document.getElementById('modal-understand');
+var dogovorContent = document.getElementById('dogovor-content');
 
 // Открываем модальное окно при клике на кнопку Договор
 modalOpenBtn.onclick = function() {
-    modal.style.display = "block";
+    // Выполняем AJAX запрос
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://enver-isliamov.github.io/Kopchek/Dogovor.html');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            dogovorContent.innerHTML = xhr.responseText; // Вставляем полученный HTML в модальное окно
+            modal.style.display = "block"; // Показываем модальное окно
+        } else {
+            alert('Произошла ошибка при загрузке договора');
+        }
+    };
+    xhr.send();
 }
 
 // Закрываем модальное окно при клике на крестик
-modalCloseBtn.onclick = function() {
-    modal.style.display = "none";
-}
+modalCloseBtn.onclick = closeModal;
 
 // Закрываем модальное окно при клике на кнопку "Понятно"
-document.querySelector('.modal-content .button').onclick = function() {
-    modal.style.display = "none";
-}
+modalUnderstandBtn.onclick = closeModal;
 
 // Закрываем модальное окно при клике вне его области
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        closeModal();
     }
+}
+
+// Функция для закрытия модального окна
+function closeModal() {
+    modal.style.display = "none";
+    dogovorContent.innerHTML = ''; // Очищаем содержимое при закрытии окна
 }
