fdd8a1f4ea7edadf52cb6f3c932d1f77d1a57e09
enver-isliamov <enverphoto@gmail.com>
2024-07-09 17:44:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-// Находим кнопку Договор и модальное окно
+// Находим кнопку Договор
 var modalOpenBtn = document.getElementById('modal-open');
-var modal = document.getElementById('modal');
-var modalCloseBtn = document.getElementById('modal-close');
-var docFrame = document.getElementById('doc-frame');
 
-// Открываем модальное окно при клике на кнопку Договор
+// Открываем ссылку на Dogovor.html при клике на кнопку Договор
 modalOpenBtn.onclick = function() {
-    modal.style.display = 'block';
-    // Загружаем содержимое Google Docs в iframe
-    docFrame.src = 'https://enver-isliamov.github.io/Dogovor';
-}
-
-// Закрываем модальное окно при клике на крестик
-modalCloseBtn.onclick = function() {
-    modal.style.display = 'none';
-}
-
-// Закрываем модальное окно при клике вне области модального окна
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = 'none';
-    }
+    window.open('https://enver-isliamov.github.io/Kopchek/Dogovor.html', '_blank');
 }
