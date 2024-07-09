a7fdf04148ca3e1289c2b3888b7a2aca3beb1391
enver-isliamov <enverphoto@gmail.com>
2024-07-09 18:26:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-// Находим кнопку Договор и модальное окно
-var modalOpenBtn = document.getElementById('modal-open');
+// Get the modal element
 var modal = document.getElementById('modal');
-var modalCloseBtn = document.getElementById('modal-close');
 
-// Открываем модальное окно при клике на кнопку Договор
-modalOpenBtn.onclick = function() {
+// Get the button that opens the modal
+var btn = document.getElementById("modal-open");
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("modal-close");
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// Закрываем модальное окно при клике на крестик
-modalCloseBtn.onclick = function() {
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
     modal.style.display = "none";
 }
 
-// Закрываем модальное окно при клике на кнопку "Понятно"
-document.querySelector('.modal-content .button').onclick = function() {
-    modal.style.display = "none";
-}
-
-// Закрываем модальное окно при клике вне его области
+// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
