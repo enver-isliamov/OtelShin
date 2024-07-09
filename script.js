f6fc4a99b914f39204f7039951125574adefc339
enver-isliamov <enverphoto@gmail.com>
2024-07-09 18:32:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-// Get the modal element
-var modal = document.getElementById('modal');
+// JavaScript для открытия и закрытия модального окна
+        var modal = document.getElementById("myModal");
+        var btnOpen = document.getElementById("modal-open");
+        var btnClose = document.getElementById("modal-close");
 
-// Get the button that opens the modal
-var btn = document.getElementById("modal-open");
+        // Открытие модального окна при клике на кнопку
+        btnOpen.onclick = function() {
+            modal.style.display = "block";
+        }
 
-// Get the <span> element that closes the modal
-var span = document.getElementById("modal-close");
+        // Закрытие модального окна при клике на кнопку закрытия
+        btnClose.onclick = function() {
+            modal.style.display = "none";
+        }
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+        // Закрытие модального окна при клике вне окна
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
