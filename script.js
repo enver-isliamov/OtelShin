d9ac183c72d79ac6634455df1c9c47171eb43808
enver-isliamov <enverphoto@gmail.com>
2024-07-09 17:07:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 <script>
-    // Находим кнопку Договор и модальное окно
-    var modalOpenBtn = document.getElementById('modal-open');
-    var modal = document.getElementById('modal');
-    var modalCloseBtn = document.getElementById('modal-close');
+        // Находим кнопку Договор и модальное окно
+        var modalOpenBtn = document.getElementById('modal-open');
+        var modal = document.getElementById('modal');
+        var modalCloseBtn = document.getElementById('modal-close');
 
-    // Открываем модальное окно при клике на кнопку Договор
-    modalOpenBtn.onclick = function() {
-        modal.classList.add('active');
-    }
+        // Открываем модальное окно при клике на кнопку Договор
+        modalOpenBtn.onclick = function() {
+            modal.classList.add('active');
+        }
 
-    // Закрываем модальное окно при клике на крестик
-    modalCloseBtn.onclick = function() {
-        modal.classList.remove('active');
-    }
-
-    // Закрываем модальное окно при клике за его пределами
-    window.onclick = function(event) {
-        if (event.target == modal) {
+        // Закрываем модальное окно при клике на крестик
+        modalCloseBtn.onclick = function() {
             modal.classList.remove('active');
         }
-    }
-</script>
+
+        // Закрываем модальное окно при клике за его пределами
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.classList.remove('active');
+            }
+        }
+    </script>
