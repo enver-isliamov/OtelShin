af455eb2ee9116e9a2eb3a7cec66f49746947554
enver-isliamov <enverphoto@gmail.com>
2024-07-09 17:23:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,23 @@
 var modalOpenBtn = document.getElementById('modal-open');
 var modal = document.getElementById('modal');
 var modalCloseBtn = document.getElementById('modal-close');
+var docFrame = document.getElementById('doc-frame');
 
 // Открываем модальное окно при клике на кнопку Договор
 modalOpenBtn.onclick = function() {
-    modal.classList.add('active');
+    modal.style.display = 'block';
+    // Загружаем содержимое Google Docs в iframe
+    docFrame.src = 'https://docs.google.com/document/d/1r7_mNG0VjecHp2Z4rN7ft0oh7-PQLEpkpsAqtC_P8fw/edit?usp=sharing';
 }
 
 // Закрываем модальное окно при клике на крестик
 modalCloseBtn.onclick = function() {
-    modal.classList.remove('active');
+    modal.style.display = 'none';
 }
 
-// Закрываем модальное окно при клике за его пределами
+// Закрываем модальное окно при клике вне области модального окна
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.classList.remove('active');
+        modal.style.display = 'none';
     }
 }
