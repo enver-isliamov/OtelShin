7aca8a1362dd39d241564bcc03b307a78de480d9
enver-isliamov <enverphoto@gmail.com>
2024-11-16 19:05:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,33 @@
                 modal.style.display = "none";
             }
         }
+
+// Функция частые вопросы открываются и смена +/-
+function toggleFaq(element) {
+    const content = element.nextElementSibling;
+    const sign = element.querySelector('span:last-child'); // Получаем элемент с символом +/-
+
+    // Закрываем все остальные ответы
+    const allHeaders = document.querySelectorAll('.faq-header');
+    
+    allHeaders.forEach(header => {
+        const headerContent = header.nextElementSibling;
+
+        if (headerContent !== content) {
+            headerContent.style.display = 'none';
+            header.classList.remove('active'); // Убираем активный класс
+            header.querySelector('span:last-child').textContent = '+'; // Возвращаем знак +
+        }
+        else if (headerContent === content) {
+            // Переключаем видимость текущего ответа и добавляем/убираем активный класс
+            const isVisible = content.style.display === 'block';
+            content.style.display = isVisible ? 'none' : 'block';
+            header.classList.toggle('active', !isVisible); // Устанавливаем класс в зависимости от состояния
+            
+            // Меняем знак на + или -
+            sign.textContent = isVisible ? '+' : '-';
+        }
+    });
+}
+// <-- Функция частые вопросы открываются и смена +/-
+
